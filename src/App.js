d1c2aec8228262a7c71c74cad25fb78f63976ada
AndresD <andresd@pop-os.localdomain>
2019-08-20 20:32:38 -0500

    sass

--- src/App.js
+++ src/App.js
@@ -4,21 +4,20 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     <nav>
+       <ul>
+         <li>
+         <a href= "#" >home </a>
+         </li>
+         <li>
+         <a href= "#" >about </a>
+         </li>
+         <li>
+         <a href= "#" >contact </a>
+         </li>
+       </ul>
+     </nav>
     </div>
   );
 }
