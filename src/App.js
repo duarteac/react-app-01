227d773297dd1ac57d46d58058fae953819bdc0c
AndresD <andresd@pop-os.localdomain>
2019-08-27 20:30:27 -0500

    3 commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
+/*import logo from './logo.svg';*/
 import './App.css';
 
 function App() {
   return (
+    <main className="main">
+      <header className="header">
     <div>
-     <nav>
-       <ul>
-         <li>
-         <a href= "#" >home </a>
+     <nav className="nav">
+       <ul className="menu">
+         <li className="list-menu">
+         <a href= "#" className="list-menu-link" >home </a>
          </li>
-         <li>
-         <a href= "#" >about </a>
+         <li className="list-menu">
+         <a href= "#"  className="list-menu-link">about </a>
          </li>
-         <li>
-         <a href= "#" >contact </a>
+         <li className="list-menu">
+         <a href= "#"  className="list-menu-link">contact </a>
          </li>
        </ul>
      </nav>
     </div>
+    </header>
+    <aside className="aside">
+      <ul className="menu">
+        <li className="list-menu"><h2>lo mas comentado</h2></li>
+        <li className="list-menu"><h2>lo mas visto</h2></li>
+      </ul>
+    </aside>
+
+    <section className="section">
+
+    </section>
+
+    <footer className="footer">
+        <p className="copyright">todos los derechos reservados, 2019</p>
+    </footer>
+    </main>
   );
 }
 
