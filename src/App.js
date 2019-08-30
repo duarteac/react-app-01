c8c844dd21573d65f2035aaa033c6ce4f438b299
AndresD <andresd@pop-os.localdomain>
2019-08-29 20:23:23 -0500

    otros arreglos

--- src/App.js
+++ src/App.js
@@ -21,22 +21,57 @@ function App() {
        </ul>
      </nav>
     </div>
+
+    <div class = "main-info">
+    <h1 class ="title" >Design<spam class ="title-light">Agency</spam></h1>
+    <p class="subline">make your dreams come true</p>
+    <form className="form form-header"><button className="buttom"> get started</button></form>
+    </div>
     </header>
-    <aside className="aside">
-      <ul className="menu">
-        <li className="menu-item"><h2>lo mas comentado</h2></li>
-        <li className="menu-item"><h2>lo mas visto</h2></li>
-      </ul>
-    </aside>
 
-    <section className="section">
+    <section className="section-one">
 
-    <h2>Title</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
-            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
-            
+    <h2 class="hidden">Title</h2>
+        
     </section>
+    <section className="section-two">
+
+<h2>Title</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+        
+</section>
+<section className="section-three">
+
+<h2 className= "hiden">Title</h2>
+    <ul className="creative">
+    <li className="creative-item">
+        <article>
+          <p className="image" img scr="">
+            <h3 className="title">creatice minds</h3>
+
+          </p>
+        </article>
+      </li>
+      <li className="creative-item">
+        <article>
+          <p className="image" img scr="">
+            <h3 className="title">creatice minds</h3>
+
+          </p>
+        </article>
+      </li>
+      <li className="creative-item">
+        <article>
+          <p className="image" img scr="">
+            <h3 className="title">creatice minds</h3>
+
+          </p>
+        </article>
+      </li>
+    </ul>
+</section>
 
     <footer className="footer">
         <p className="copyright">todos los derechos reservados, 2019</p>
